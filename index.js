d8f90e483596c46d0e3be1015b65c791c19e6366
MCavieres <maca.c.91@gmail.com>
2024-04-17 14:35:17 -0400

    se hacen ejercicios como practica

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { products } from "./products.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send({ msg: "Helloucito" });
+});
+
+app.get("/TocToc", (req, res) => {
+    res.send("Quien es??");
+});
+
+app.get("/products/", (req, res) => {
+    res.json(products);
+});
+
+app.get("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const requerido = products.find((item) => item.id === +id);
+    if (!requerido) return res.send("Producto no encontrado");
+
+    return res.json(requerido);
+});
+
+app.get("/azar/:numero", (req, res) => {
+    const { numero } = req.params;
+    const aleatorio = Math.ceil(Math.random() * 3);
+    +numero === aleatorio
+        ? res.send("<center><h1>Hoy estas de suerte ;)</h1></center>")
+        : res.send("<center><h1>Buena suerte para la próxima</h1></center>");
+});
+
+app.use("*", (req, res) => {
+    res.send("<center><h1>Sorry, aquí no hay nada :/</h1></center>");
+});
+
+app.listen(PORT, () => {
+    console.log(`el puerto ${PORT} esta escuchando; http://localhost:${PORT}`);
+});
---
+++ products.js
@@ -0,0 +1,27 @@
+export const products = [
+    {
+        id: 1,
+        product: "p1",
+        price: 200,
+    },
+    {
+        id: 2,
+        product: "p2",
+        price: 100,
+    },
+    {
+        id: 3,
+        product: "p3",
+        price: 500,
+    },
+    {
+        id: 4,
+        product: "p4",
+        price: 200,
+    },
+    {
+        id: 5,
+        product: "p5",
+        price: 800,
+    },
+];
